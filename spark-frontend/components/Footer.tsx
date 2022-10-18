be72c4aa7253dfcbd3cae219a3bf21e268ab2bf1
Keith <keithching91@gmail.com>
2022-10-18 19:29:59 +0900

    style: adjust footer

--- spark-frontend/components/Footer.tsx
+++ spark-frontend/components/Footer.tsx
@@ -4,41 +4,44 @@ import Image from 'next/image';
 const Footer = () => {
     return (
         <div className={footerStyles.Footer}>
-            <div className={footerStyles.link}>
-                <a href="https://github.com/keithching" target="_blank" rel="noreferrer">
-                    <Image
-                        priority
-                        // layout="fill"
-                        width={50}
-                        height={50}
-                        src="/images/github.png"
-                        alt="github"
-                    />
-                </a>
-            </div>
-            <div className={footerStyles.link}>
-                <a href="https://twitter.com/KeithNgPC" target="_blank" rel="noreferrer">
-                    <Image
-                        priority
-                        // layout="fill"
-                        width={50}
-                        height={50}
-                        src="/images/twitter.png"
-                        alt="twitter"
-                    />
-                </a>
-            </div>
-            <div className={footerStyles.link}>
-                <a href="https://www.linkedin.com/in/keith-pak-chung-ng/" target="_blank" rel="noreferrer">
-                    <Image
-                        priority
-                        // layout="fill"
-                        width={50}
-                        height={50}
-                        src="/images/linkedin.png"
-                        alt="linkedin"
-                    />
-                </a>
+            <p>Created by Keith Ng</p>
+            <div className={footerStyles.links}>
+                <div className={footerStyles.link}>
+                    <a href="https://github.com/keithching" target="_blank" rel="noreferrer">
+                        <Image
+                            priority
+                            // layout="fill"
+                            width={30}
+                            height={30}
+                            src="/images/github.png"
+                            alt="github"
+                        />
+                    </a>
+                </div>
+                <div className={footerStyles.link}>
+                    <a href="https://twitter.com/KeithNgPC" target="_blank" rel="noreferrer">
+                        <Image
+                            priority
+                            // layout="fill"
+                            width={30}
+                            height={30}
+                            src="/images/twitter.png"
+                            alt="twitter"
+                        />
+                    </a>
+                </div>
+                <div className={footerStyles.link}>
+                    <a href="https://www.linkedin.com/in/keith-pak-chung-ng/" target="_blank" rel="noreferrer">
+                        <Image
+                            priority
+                            // layout="fill"
+                            width={30}
+                            height={30}
+                            src="/images/linkedin.png"
+                            alt="linkedin"
+                        />
+                    </a>
+                </div>
             </div>
         </div>
     );
