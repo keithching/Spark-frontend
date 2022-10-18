936deb6630c68e42f10ad46a894f9c6d67aba040
Keith <keithching91@gmail.com>
2022-10-18 22:55:48 +0900

    fix: static assets for nextjs build

--- spark-frontend/pages/_document.tsx
+++ spark-frontend/pages/_document.tsx
@@ -8,14 +8,14 @@ export default function Document() {
         <meta charSet="utf-8" />
         {/* https://stackoverflow.com/questions/56213019/how-to-add-a-favicon-to-a-next-js-static-site */}
         {/* https://github.com/vercel/next.js/blob/deprecated-main/errors/static-dir-deprecated.md */}
-        <link rel="icon" href="/static/favicon.ico" />
+        {/* <link rel="icon" href="/static/favicon.ico" /> */}
         <meta name="theme-color" content="#000000" />
         <meta
           name="description"
           content="Spark frontend built using Next.js"
         />
-        <link rel="apple-touch-icon" href="/static/logo192.png" />
-        <link rel="manifest" href="/static/manifest.json" />
+        {/* <link rel="apple-touch-icon" href="/static/logo192.png" /> */}
+        {/* <link rel="manifest" href="/static/manifest.json" /> */}
         {/* https://nextjs.org/docs/basic-features/font-optimization */}
         <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet" />
       </Head>
