68b60de3c0530e13c9bfb0b73109358cb3706eb0
Keith <keithching91@gmail.com>
2022-10-18 23:43:54 +0900

    fix: remove basePath for nextjs deployment

--- spark-frontend/next.config.mjs
+++ spark-frontend/next.config.mjs
@@ -4,8 +4,6 @@
  const nextConfig = {
     /* config options here */
     images: { domains: ['firebasestorage.googleapis.com'], },
-    // https://creativedesignsguru.com/deploy-your-next-js-application-in-subfolder/
-    basePath: '/spark-frontend'
   }
   
   export default nextConfig
