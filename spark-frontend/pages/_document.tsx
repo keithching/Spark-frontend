baf0f15a5a52597df808e8aa539fdb94d6d3bb72
Keith <keithching91@gmail.com>
2022-10-18 22:46:49 +0900

    fix: static assets for nextjs build

--- spark-frontend/pages/_document.tsx
+++ spark-frontend/pages/_document.tsx
@@ -6,14 +6,15 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
+        {/* https://stackoverflow.com/questions/56213019/how-to-add-a-favicon-to-a-next-js-static-site */}
+        <link rel="icon" href="/static/favicon.ico" />
         <meta name="theme-color" content="#000000" />
         <meta
           name="description"
           content="Spark frontend built using Next.js"
         />
-        <link rel="apple-touch-icon" href="/logo192.png" />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/static/logo192.png" />
+        <link rel="manifest" href="/static/manifest.json" />
         {/* https://nextjs.org/docs/basic-features/font-optimization */}
         <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet" />
       </Head>
