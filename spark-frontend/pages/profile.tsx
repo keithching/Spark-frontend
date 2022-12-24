8d4229569a67ff747c5fd5ee0c62273367c0567c
Keith <keithching91@gmail.com>
2022-12-24 15:18:34 +0900

    feat: add event listener for profile image

--- spark-frontend/pages/profile.tsx
+++ spark-frontend/pages/profile.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useAuth } from '../contexts/AuthContext';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -10,7 +10,8 @@ import Image from 'next/image';
 export default function Profile() {
     const router = useRouter();
     const [error, setError] = useState<string>("");
-    const { currentUser, logout } = useAuth();
+    const { currentUser, logout, updatePhotoURL } = useAuth();
+    const profileImageRef = useRef(null);
 
     async function handleLogout() {
         setError('');
@@ -21,6 +22,38 @@ export default function Profile() {
             setError('Failed to log out');
         }
     }
+
+    const [photoURL, setPhotoURL] = useState<string>("");
+    // useEffect(() => {
+    //     if (currentUser.photoURL) {
+    //         console.log(currentUser.photoURL);
+    //         setPhotoURL(currentUser.photoURL);
+    //     }
+    // }, [currentUser]);
+
+    const [isHover, setIsHover] = useState<boolean>(false);
+    const [isClick, setIsClick] = useState<boolean>(false);
+
+    const handleProfilePhotoMouseOver = () => {
+        setIsHover(true);
+        profileImageRef.current.classList.add(`${profileStyle["profile-image-hover"]}`);
+    }
+
+    const handleProfilePhotoMouseOut = () => {
+        setIsHover(false);
+        profileImageRef.current.classList.remove(`${profileStyle["profile-image-hover"]}`);
+    }
+
+    const handleProfilePhotoClick = () => {
+        setIsClick(true);
+    }
+
+    useEffect(() => {
+        if (isClick) {
+            console.log('fish!!');
+            setIsClick(false);
+        }
+    }, [isClick]);
 
   return (
       <Layout>
@@ -42,10 +75,19 @@ export default function Profile() {
                         <button onClick={handleLogout}>Log Out</button>
                     </div>
                 </div>
-                <div className={profileStyle["profile-image"]}>
+                <div 
+                    className={profileStyle["profile-image"]}
+                    onMouseOver={handleProfilePhotoMouseOver}
+                    onMouseOut={handleProfilePhotoMouseOut}
+                    onClick={handleProfilePhotoClick}
+                    ref={profileImageRef}
+                >
+                    {isHover ? 
+                        <div className={profileStyle["profile-image-upload-text"]}>upload</div>
+                        : null
+                    }
                     <Image 
-                        // src={currentUser.imageURL} 
-                        src={""}
+                        src={photoURL}
                         alt="" 
                         width={400}
                         height={400}
