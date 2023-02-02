cf418c06ef77e7b621a75efc416aba66c708eaa1
Keith <keithching91@gmail.com>
2023-02-02 11:12:12 +0900

    feat: prep data output from cart

--- spark-frontend/components/Popup.tsx
+++ spark-frontend/components/Popup.tsx
@@ -1,23 +1,39 @@
-import { useEffect, useRef } from 'react';
-import PopupStyles from '../styles/popup.module.css';
-import { useCart } from '../utils/store';
+import { useEffect, useState } from "react";
+import PopupStyles from "../styles/popup.module.css";
+import { useEvents } from "../utils/helper";
+import { useCart } from "../utils/store";
 
 export const Popup = ({ data }) => {
-    const counter = useCart((state) => state.counter);
-    const handleSendEventsBtnClick = () => {
-        console.log('implement sending events to server');
-    };
+  const counter = useCart((state) => state.counter);
+  const eventIdsInCart = useCart((state) => state.events); // eventIds
+  const { events, isError, isLoading } = useEvents();
+  const [eventsInCart, setEventsInCart] = useState([]);
 
-    return (
-        <div className={PopupStyles["popup"]}>
-            <div className={PopupStyles["popup-text"]}>
-                {data}
-                <button 
-                    disabled={counter === 0}
-                    className={PopupStyles.sendEventsToServerBtn}
-                    onClick={handleSendEventsBtnClick}
-                >send to server</button>
-            </div>
-        </div>
-    );
-}
+  useEffect(() => {
+    if (eventIdsInCart && events) {
+      setEventsInCart(
+        eventIdsInCart.map((id) => events.find((event) => event.id === id))
+      );
+    }
+  }, [eventIdsInCart, events]);
+
+  const handleSendEventsBtnClick = () => {
+    console.log("implement sending events to server");
+    console.log(eventsInCart);
+  };
+
+  return (
+    <div className={PopupStyles["popup"]}>
+      <div className={PopupStyles["popup-text"]}>
+        {data}
+        <button
+          disabled={counter === 0}
+          className={PopupStyles.sendEventsToServerBtn}
+          onClick={handleSendEventsBtnClick}
+        >
+          send to server
+        </button>
+      </div>
+    </div>
+  );
+};
