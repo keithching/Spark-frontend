1719402e01624b7b1c6638e67b97e09678ec91e3
Keith <keithching91@gmail.com>
2023-02-07 20:25:23 +0900

    feat: implement role badge at profile page

--- spark-frontend/pages/signup.tsx
+++ spark-frontend/pages/signup.tsx
@@ -1,103 +1,162 @@
-import Head from 'next/head';
-import React, { useRef, useState } from 'react';
-import signupStyles from '../styles/signup.module.css';
-import { useAuth } from '../contexts/AuthContext';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { getEventProviders, createEventProvider } from '../utils/helper';
-import Layout from '../components/layout';
+import Head from "next/head";
+import React, { useEffect, useRef, useState } from "react";
+import signupStyles from "../styles/signup.module.css";
+import { useAuth } from "../contexts/AuthContext";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import {
+  getEventProviders,
+  createEventProvider,
+  createEventConsumer,
+} from "../utils/helper";
+import Layout from "../components/layout";
+
+type Role = string;
 
 export default function Signup() {
-    const emailRef = useRef<HTMLInputElement>(null);
-    const passwordRef = useRef<HTMLInputElement>(null);
-    const passwordConfirmRef = useRef<HTMLInputElement>(null);
-    const nameRef = useRef<HTMLInputElement>(null);
-    const { signup, updateDisplayName } = useAuth();
-    const [ error, setError ] = useState<string>('');
-    const [ loading, setLoading ] = useState<boolean>(false);
-    const router = useRouter();
+  const roleRef = useRef<HTMLSelectElement>(null);
+  const [role, setRole] = useState<Role>("");
 
-    async function handleSubmit(e) {
-        e.preventDefault();
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const passwordConfirmRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const { signup, updateDisplayName } = useAuth();
+  const [error, setError] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const router = useRouter();
 
-        // check uniqueness of user input name
-        const eventProviderNames = (await getEventProviders()).map(provider => provider.name);
-        
-        if (eventProviderNames.includes(nameRef.current.value)) {
-            return setError('Name is already taken');
-        }
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-            return setError('Password do not match');
-        }
-
-        try {
-            setError('');
-            setLoading(true);
-            await signup(emailRef.current.value, passwordRef.current.value);
-
-            await updateDisplayName(nameRef.current.value);
-
-            // push this event provider data to the server database
-            await createEventProvider({
-                name: nameRef.current.value,
-                email: emailRef.current.value,
-                password: passwordRef.current.value // to refactor. take away password field from database. use firebase auth entirely instead
-            });
-
-            // router.push('/dashboard');
-            router.push('/profile'); // redirect to profile page for setting up 
-        } catch(err) {
-            console.error(err);
-            setError('Failed to create an account');
-        }
-
-        setLoading(false);
+    // check if a role has been selected
+    if (role !== "provider" && role !== "consumer") {
+      return setError("Please pick a role");
     }
 
-    return (
-        <Layout>
-            <Head>
-                <title>Signup</title>
-            </Head>
-            <div className={signupStyles["signup-container"]}>
-                <div className={signupStyles["signup-card"]}>
-                    <h2 className={signupStyles["signup-title"]}>Sign Up</h2>
-                    {error && <span>{error}</span>}
-                    <form action="" className={signupStyles["signup-form"]} onSubmit={handleSubmit}>
-                        {/* <label htmlFor="nameInput">Name</label> */}
-                        <input 
-                            type="text" 
-                            ref={nameRef} 
-                            id="nameInput" 
-                            required autoComplete="off" 
-                            placeholder='name'
-                        />
-                        {/* <label htmlFor="emailInput">Email</label> */}
-                        <input 
-                            type="email" ref={emailRef} 
-                            id="emailInput" required autoComplete="off" 
-                            placeholder='email'
-                        />
-                        {/* <label htmlFor="passwordInput">Password</label> */}
-                        <input 
-                            type="password" ref={passwordRef} 
-                            id="passwordInput" required 
-                            placeholder='password'    
-                        />
-                        {/* <label htmlFor="passwordConfirmInput">Password Confirmation</label> */}
-                        <input 
-                            type="password" ref={passwordConfirmRef} 
-                            id="passwordConfirmInput" required 
-                            placeholder='password again'
-                        />
-                        <button disabled={loading} type="submit">Sign Up</button>
-                    </form>
-                </div>
-                <div className={signupStyles["sign-up-link"]}>
-                    Already have an account? <Link href="/login">Log In</Link>
-                </div>
-            </div>
-        </Layout>
-  )
+    // check uniqueness of user input name
+    const eventProviderNames = (await getEventProviders()).map(
+      (provider) => provider.name
+    );
+
+    if (eventProviderNames.includes(nameRef.current.value)) {
+      return setError("Name is already taken");
+    }
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError("Password do not match");
+    }
+
+    try {
+      setError("");
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+
+      await updateDisplayName(nameRef.current.value);
+
+      if (role === "provider") {
+        // push this event provider data to the server database
+        await createEventProvider({
+          name: nameRef.current.value,
+          email: emailRef.current.value,
+        });
+      } else if (role === "consumer") {
+        await createEventConsumer({
+          name: nameRef.current.value,
+          email: emailRef.current.value,
+        });
+      }
+
+      // router.push('/dashboard');
+      router.push("/profile"); // redirect to profile page for setting up
+    } catch (err) {
+      console.error(err);
+      setError("Failed to create an account");
+    }
+
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setError("");
+    }, 2000);
+  }, [error]);
+
+  const handleRoleSelection = () => {
+    setRole(roleRef.current.value);
+  };
+
+  return (
+    <Layout>
+      <Head>
+        <title>Signup</title>
+      </Head>
+      <div className={signupStyles["signup-container"]}>
+        <div className={signupStyles["signup-card"]}>
+          <h2 className={signupStyles["signup-title"]}>Sign Up</h2>
+          {error && <span>{error}</span>}
+          <select
+            name="roleSelection"
+            id=""
+            ref={roleRef}
+            onChange={handleRoleSelection}
+            defaultValue="pickRole"
+          >
+            <option value="pickRole" disabled>
+              pick a role
+            </option>
+            <option value="provider">provider</option>
+            <option value="consumer">consumer</option>
+          </select>
+          <form
+            action=""
+            className={signupStyles["signup-form"]}
+            onSubmit={handleSubmit}
+          >
+            {/* <label htmlFor="nameInput">Name</label> */}
+            <input
+              type="text"
+              ref={nameRef}
+              id="nameInput"
+              required
+              autoComplete="off"
+              placeholder="name"
+            />
+            {/* <label htmlFor="emailInput">Email</label> */}
+            <input
+              type="email"
+              ref={emailRef}
+              id="emailInput"
+              required
+              autoComplete="off"
+              placeholder="email"
+            />
+            {/* <label htmlFor="passwordInput">Password</label> */}
+            <input
+              type="password"
+              ref={passwordRef}
+              id="passwordInput"
+              required
+              placeholder="password"
+            />
+            {/* <label htmlFor="passwordConfirmInput">Password Confirmation</label> */}
+            <input
+              type="password"
+              ref={passwordConfirmRef}
+              id="passwordConfirmInput"
+              required
+              placeholder="password again"
+            />
+            <button disabled={loading} type="submit">
+              Sign Up
+            </button>
+          </form>
+        </div>
+        <div className={signupStyles["sign-up-link"]}>
+          Already have an account? <Link href="/login">Log In</Link>
+        </div>
+      </div>
+    </Layout>
+  );
 }
--- spark-frontend/utils/helper.ts
+++ spark-frontend/utils/helper.ts
@@ -1,16 +1,17 @@
-import axios from 'axios';
-import jpPrefecture from 'jp-prefecture';
+import axios from "axios";
+import jpPrefecture from "jp-prefecture";
 // https://github.com/wadackel/jp-prefecture
 
 // client-side data fetching with swr
 // https://swr.vercel.app/docs/getting-started
-import useSWR from 'swr';
+import useSWR from "swr";
 
-const URL = 'https://spark-backend-app.herokuapp.com/api';
+const URL = "https://spark-backend-app.herokuapp.com/api";
 // const URL = 'http://localhost:4000/api';
 
-// spark backend 
+// spark backend
 const EVENT_PROVIDER_URL = `${URL}/event_providers`;
+const EVENT_CONSUMER_URL = `${URL}/event_consumers`;
 const EVENT_CATEGORY_URL = `${URL}/event_categories`;
 const EVENT_URL = `${URL}/events`;
 
@@ -20,129 +21,169 @@ const EVENT_URL = `${URL}/events`;
 
 // https://stackoverflow.com/questions/34179897/typescript-and-spread-operator
 // @ts-ignore
-const fetcher = (...args) => fetch(...args).then(res => res.json());
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 // getting event provider by email
 export function useEventProvider(email) {
-    const { data, error, isLoading } = useSWR(`${EVENT_PROVIDER_URL}/${email}`, fetcher);
-    
-    return {
-        eventProvider: data,
-        isLoading,
-        isError: error
-    }
+  const { data, error, isLoading } = useSWR(
+    `${EVENT_PROVIDER_URL}/${email}`,
+    fetcher
+  );
+
+  return {
+    eventProvider: data,
+    isLoadingEP: isLoading,
+    isErrorEP: error,
+  };
+}
+
+export function useEventConsumer(email) {
+  const { data, error, isLoading } = useSWR(
+    `${EVENT_CONSUMER_URL}/${email}`,
+    fetcher
+  );
+
+  return {
+    eventConsumer: data,
+    isLoadingEC: isLoading,
+    isErrorEC: error,
+  };
 }
 
 const getEventProviders = async () => {
-    const eventProviders = await axios.get(EVENT_PROVIDER_URL)
-        .then(res => res.data);
-    return eventProviders;
+  const eventProviders = await axios
+    .get(EVENT_PROVIDER_URL)
+    .then((res) => res.data);
+  return eventProviders;
 };
 
 const getEventCategories = async () => {
-    const eventCategories = await axios.get(EVENT_CATEGORY_URL).then(res => res.data);
-    return eventCategories;
+  const eventCategories = await axios
+    .get(EVENT_CATEGORY_URL)
+    .then((res) => res.data);
+  return eventCategories;
 };
 
 const getEvents = async () => {
-    const events = await axios.get(EVENT_URL).then(res => res.data);
-    return events;
+  const events = await axios.get(EVENT_URL).then((res) => res.data);
+  return events;
 };
 
 export function useEvents() {
-    const { data, error, isLoading } = useSWR(`${EVENT_URL}`, fetcher);
-    
-    return {
-        events: data,
-        isLoading,
-        isError: error
-    }
+  const { data, error, isLoading } = useSWR(`${EVENT_URL}`, fetcher);
+
+  return {
+    events: data,
+    isLoading,
+    isError: error,
+  };
 }
 
 const getAllEventIds = async () => {
-    const events = await axios.get(EVENT_URL).then(res => res.data);
-    const ids = events.map(event => {
-        return {
-            params: {
-                id: event.id.toString()
-            }
-        }
-    });
-    return ids;
-}
+  const events = await axios.get(EVENT_URL).then((res) => res.data);
+  const ids = events.map((event) => {
+    return {
+      params: {
+        id: event.id.toString(),
+      },
+    };
+  });
+  return ids;
+};
 
 const getEventsByEmail = async (email: string) => {
-        const events = await axios.get(`${EVENT_URL}/${email}`)
-        .then(res => res.data)
-        .catch(err => console.log(err));
-        return events;
+  const events = await axios
+    .get(`${EVENT_URL}/${email}`)
+    .then((res) => res.data)
+    .catch((err) => console.log(err));
+  return events;
 };
 
 const getEventByEventId = async (id: string | number) => {
-    const event = await axios.get(`${EVENT_URL}/${id}`).then(res => res.data);
-    return event[0];
+  const event = await axios.get(`${EVENT_URL}/${id}`).then((res) => res.data);
+  return event[0];
 };
 
 const getAllRegions = () => {
-    return jpPrefecture.getAllRegion("name");
+  return jpPrefecture.getAllRegion("name");
 };
 
 const getAllPrefectures = () => {
-    return jpPrefecture.getAllPref("name");
+  return jpPrefecture.getAllPref("name");
+};
+
+const createEventConsumer = async (eventConsumer: object) => {
+  try {
+    await axios
+      .post(EVENT_CONSUMER_URL, eventConsumer)
+      .then((res) => console.log(res));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 const createEventProvider = async (eventProvider: object) => {
-    try {
-        await axios.post(EVENT_PROVIDER_URL, eventProvider).then(res => console.log(res));
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    await axios
+      .post(EVENT_PROVIDER_URL, eventProvider)
+      .then((res) => console.log(res));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
-const updateEventProviderByEmail = async (email: string, eventProvider: object) => {
-    try {
-        await axios.patch(`${EVENT_PROVIDER_URL}/${email}`, eventProvider).then(res => console.log(res));
-    } catch (err) {
-        console.error(err);
-    }
-}
+const updateEventProviderByEmail = async (
+  email: string,
+  eventProvider: object
+) => {
+  try {
+    await axios
+      .patch(`${EVENT_PROVIDER_URL}/${email}`, eventProvider)
+      .then((res) => console.log(res));
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 const createEvent = async (event) => {
-    try {
-        await axios.post(EVENT_URL, event).then(res => console.log(res));
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    await axios.post(EVENT_URL, event).then((res) => console.log(res));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 const updateEvent = async (event, id: string | number) => {
-    try {
-        await axios.patch(`${EVENT_URL}/${id}`, event).then(res => console.log(res));
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    await axios
+      .patch(`${EVENT_URL}/${id}`, event)
+      .then((res) => console.log(res));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 const deleteEvent = async (id: string | number) => {
-    try {
-        await axios.delete(`${EVENT_URL}/${id}`).then(res => console.log(res));
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    await axios.delete(`${EVENT_URL}/${id}`).then((res) => console.log(res));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export {
-    getEventProviders,
-    getEventCategories,
-    getEvents,
-    getAllEventIds,
-    getEventsByEmail,
-    getEventByEventId,
-    getAllRegions,
-    getAllPrefectures,
-    createEventProvider,
-    updateEventProviderByEmail,
-    createEvent,
-    updateEvent,
-    deleteEvent
+  getEventProviders,
+  getEventCategories,
+  getEvents,
+  getAllEventIds,
+  getEventsByEmail,
+  getEventByEventId,
+  getAllRegions,
+  getAllPrefectures,
+  createEventConsumer,
+  createEventProvider,
+  updateEventProviderByEmail,
+  createEvent,
+  updateEvent,
+  deleteEvent,
 };
