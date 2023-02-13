ccbab8504b982abddb937d4da203372b688e3ecd
Keith <keithching91@gmail.com>
2023-02-13 18:08:31 +0900

    feat: materialUI tooltip for event joiner avatar tooltip

--- spark-frontend/components/EventJoiners.tsx
+++ spark-frontend/components/EventJoiners.tsx
@@ -1,13 +1,45 @@
 import Image from "next/image";
+import { forwardRef } from "react";
 import classNames from "classnames";
 import { useEventsJoinEventConsumerByEventId } from "../utils/helper";
 import eventJoinerStyles from "../styles/eventJoiners.module.css";
+// React Tooltip from MUI
+// https://mui.com/material-ui/react-tooltip/
+import Tooltip from "@mui/material/Tooltip";
+import Zoom from "@mui/material/Zoom";
+
+interface Props {
+  joiner: {
+    profile_pic_url: string;
+    consumer_name: string;
+  };
+}
+
+type Ref = HTMLDivElement;
 
 export const EventJoiners = ({ eventData }) => {
   // get the eventJoiners from backend API
   const { eventsJoinEventConsumer } = useEventsJoinEventConsumerByEventId(
     eventData?.id
   );
+
+  // forward the ref from tooltip to the image
+  const ProfilePicImage = forwardRef<Ref, Props>(function ProfilePicImage(
+    props,
+    ref
+  ) {
+    return (
+      <div {...props} ref={ref}>
+        <Image
+          src={props.joiner.profile_pic_url}
+          alt={props.joiner.consumer_name}
+          width={50}
+          height={50}
+          objectFit="cover"
+        />
+      </div>
+    );
+  });
 
   return (
     <div className={classNames(eventJoinerStyles.joinersDiv)}>
@@ -19,18 +51,15 @@ export const EventJoiners = ({ eventData }) => {
               key={joiner.consumer_id}
               className={classNames(eventJoinerStyles.joinerProfilePic)}
             >
-              {/* {joiner.consumer_name} */}
-              {/* {joiner.profile_pic_url} */}
               {joiner.profile_pic_url && (
-                // <div >
-                <Image
-                  src={joiner.profile_pic_url}
-                  alt={joiner.consumer_name}
-                  width={50}
-                  height={50}
-                  objectFit="cover"
-                />
-                // </div>
+                <Tooltip
+                  title={joiner.consumer_name}
+                  placement="bottom"
+                  arrow
+                  TransitionComponent={Zoom}
+                >
+                  <ProfilePicImage joiner={joiner} />
+                </Tooltip>
               )}
             </div>
           );
