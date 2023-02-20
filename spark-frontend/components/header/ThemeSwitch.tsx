46dda13a173d670986d1556006dc2040934da042
Keith <keithching91@gmail.com>
2023-02-20 13:40:04 +0900

    style: adjust ThemeSwitch and global css

--- spark-frontend/components/header/ThemeSwitch.tsx
+++ spark-frontend/components/header/ThemeSwitch.tsx
@@ -1,8 +1,43 @@
 import { Switch } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import { useTheme } from "../../contexts/ThemeContext";
+import themeSwitchStyles from "../../styles/themeSwitch.module.css";
+
+// https://stackoverflow.com/questions/51231297/how-to-change-size-of-toggle-switch-in-material-ui
+const MuiSwitchLarge = styled(Switch)(({ theme }) => ({
+  width: 68,
+  height: 34,
+  padding: 7,
+  "& .MuiSwitch-switchBase": {
+    margin: 1,
+    padding: 0,
+    transform: "translateX(6px)",
+    "&.Mui-checked": {
+      transform: "translateX(30px)",
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    width: 32,
+    height: 32,
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: 20 / 2,
+  },
+}));
 
 export const ThemeSwitch = () => {
   const { toggleTheme, darkTheme } = useTheme();
 
-  return <Switch checked={darkTheme} onChange={toggleTheme} color="warning" />;
+  return (
+    <>
+      <MuiSwitchLarge
+        checked={darkTheme}
+        onChange={toggleTheme}
+        color="warning"
+        className={
+          darkTheme ? themeSwitchStyles.switchChecked : themeSwitchStyles.switch
+        }
+      />
+    </>
+  );
 };
