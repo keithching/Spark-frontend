7c6ecbd5e242c7540f18431479a4ed7939ee6c9e
Keith <keithching91@gmail.com>
2023-03-09 16:01:04 +0900

    chore: clean up authBtns

--- spark-frontend/components/header/AuthBtns.tsx
+++ spark-frontend/components/header/AuthBtns.tsx
@@ -1,5 +1,4 @@
 import { useAuth } from "../../contexts/AuthContext";
-import AuthBtnsStyle from "../../styles/authBtns.module.css";
 import { LogOutBtn } from "./LogOutBtn";
 import { LogInBtn } from "./LogInBtn";
 import { SignUpBtn } from "./SignUpBtn";
